57de4cc1b780b409ca03cb727ad39e4cb52b26c3
Bill Walker <bwalker@mozilla.com>
2016-02-11 14:47:27 -0800

    scriptures must always be lists because there are around 50 cases where there are two alternatives for a given sunday

--- test/scriptures.spec.js
+++ test/scriptures.spec.js
@@ -14,41 +14,48 @@ function checkSunday(inSunday, inName) {
 	// console.log(JSON.stringify(inSunday));
 
 	if (inSunday.complementary || inSunday.semicontinuous) {
-		assert.ok(checkString(inSunday.complementary.gospel), inName);
-		assert.ok(checkString(inSunday.complementary.psalm), inName);
-		assert.ok(checkString(inSunday.complementary.first), inName);
-		assert.ok(checkString(inSunday.complementary.second), inName);
+		assert.ok(checkString(inSunday.complementary.gospel[0]), inName);
+		assert.ok(checkString(inSunday.complementary.psalm[0]), inName);
+		assert.ok(checkString(inSunday.complementary.first[0]), inName);
+		assert.ok(checkString(inSunday.complementary.second[0]), inName);
 
-		assert.ok(checkString(inSunday.semicontinuous.gospel), inName);
-		assert.ok(checkString(inSunday.semicontinuous.psalm), inName);
-		assert.ok(checkString(inSunday.semicontinuous.first), inName);
-		assert.ok(checkString(inSunday.semicontinuous.second), inName);
+		assert.ok(checkString(inSunday.semicontinuous.gospel[0]), inName);
+		assert.ok(checkString(inSunday.semicontinuous.psalm[0]), inName);
+		assert.ok(checkString(inSunday.semicontinuous.first[0]), inName);
+		assert.ok(checkString(inSunday.semicontinuous.second[0]), inName);
 
 		// the psalm is always different between semicontinuous and complementary
-		assert.notEqual(inSunday.semicontinuous.psalm, inSunday.complementary.psalm, inName + ' different psalm');
+		assert.notEqual(inSunday.semicontinuous.psalm[0], inSunday.complementary.psalm[0], inName + ' different psalm');
 		// the second and gospel readings are the same between semicontinuous and complementary
-		assert.equal(inSunday.semicontinuous.second, inSunday.complementary.second, inName + ' matching second');
-		assert.equal(inSunday.semicontinuous.gospel, inSunday.complementary.gospel, inName + ' matching gospel');
+		assert.equal(inSunday.semicontinuous.second[0], inSunday.complementary.second[0], inName + ' matching second');
+		assert.equal(inSunday.semicontinuous.gospel[0], inSunday.complementary.gospel[0], inName + ' matching gospel');
 	} else {
-		assert.ok(checkString(inSunday.gospel), inName);
-		assert.ok(checkString(inSunday.psalm), inName);
-		assert.ok(checkString(inSunday.first), inName);
-		assert.ok(checkString(inSunday.second), inName);
+		assert.ok(checkString(inSunday.gospel[0]), inName);
+		assert.ok(checkString(inSunday.psalm[0]), inName);
+		assert.ok(checkString(inSunday.first[0]), inName);
+		assert.ok(checkString(inSunday.second[0]), inName);
 	}
 }
 
 describe('scriptures', function() {
-    describe('try easter 7', function() {
-    	it('has a gospel', function() {
-    		var simulatedLectionaryDay = { lectionaryYear: "B", lectionaryShortName: "Easter 7" }
-    		assert.ok(checkString(scriptures(simulatedLectionaryDay).gospel));
-    	});
-    });
+	describe('try easter 7', function() {
+		it('has a gospel', function() {
+			var simulatedLectionaryDay = { lectionaryYear: "B", lectionaryShortName: "Easter 7" }
+			assert.ok(checkString(scriptures(simulatedLectionaryDay).gospel[0]));
+		});
+	});
 
-    describe('try transfiguration', function() {
-    	it('has a gospel', function() {
-    		var simulatedLectionaryDay = { lectionaryYear: "B", lectionaryShortName: "Transfiguration" }
-    		assert.ok(checkString(scriptures(simulatedLectionaryDay).gospel));
+	describe('try year A advent 3', function() {
+		it('has two psalms', function() {
+			var simulatedLectionaryDay = { lectionaryYear: "A", lectionaryShortName: "Advent 3" }
+			assert.equal(2, scriptures(simulatedLectionaryDay).psalm.length);
+		});
+	});
+
+	describe('try transfiguration', function() {
+		it('has a gospel', function() {
+			var simulatedLectionaryDay = { lectionaryYear: "B", lectionaryShortName: "Transfiguration" }
+			assert.ok(checkString(scriptures(simulatedLectionaryDay).gospel[0]));
     	});
     });
 
